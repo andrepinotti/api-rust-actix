1cd8b4adbb860825b27199aa66bc7630b2052ff0
André Pinotti <115902096+andrepinotti@users.noreply.github.com>
2025-07-07 23:10:56 -0300

    feat: create server and first route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, web, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    
+    let port = 8080;
+        
+    HttpServer::new(|| {
+        App::new().service(index)
+    }).bind(("localhost", port))?.run().await
+
+
 }
